ec50f97e3b24db4e072273daf98dbdc7e26013c0
Natea611 <nanderson38@students.mchenry.edu>
2017-02-02 11:42:48 -0600

    Initial commit

---
+++ initializevariables/src/main/java/com/example/MyInitializeVariables.java
@@ -0,0 +1,30 @@
+package com.example;
+
+//Nate Anderson
+//Initializing and outputting variables
+
+public class MyInitializeVariables
+{
+
+    public static void main(String[] args)
+    {
+        byte byteExample = 1;
+        short shortExample = 32767;
+        int integerExample = 1100432;
+        long longExample = 999999999;
+        float floatingExample = 2;
+        double doubleExample = 2.5564;
+        boolean boolExample = false;
+        char characterExample = 'a';
+
+        System.out.println("Byte: " + byteExample);
+        System.out.println("Short: " + shortExample);
+        System.out.println("Integer: " + integerExample);
+        System.out.println("Long: " + longExample);
+        System.out.println("Float: " + floatingExample);
+        System.out.println("Double: " + doubleExample);
+        System.out.println("Boolean: " + boolExample);
+        System.out.println("Character: " + characterExample);
+    }
+
+}
